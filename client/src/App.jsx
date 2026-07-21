3422d74db23ff2c944b2c8ebd9e62175d4152653
Prince Goyal <princegyl2005@gmail.com>
2026-07-21 23:26:13 +0530

    Initial project setup with React, Vite, Tailwind and backend structure

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+ 
+
+return (
+  <>
+    <div className="min-h-screen flex flex-col justify-center">
+      <h1>Rental Management System</h1>
+
+      <p>Manage your rooms and tenants easily.</p>
+
+      <button>Login</button>
+
+      <button>Register</button>
+    </div>
+  </>
+)
+}
+
+export default App
